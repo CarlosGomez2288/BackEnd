ad3193e57013236e94b8efee38c1cd17ca350cd4
CarlosGomez2288 <cargome409@gmail.com>
2022-07-31 23:45:17 -0300

    todo

--- backend/src/main/java/com/backend/backend/Controller/CEducacion.java
+++ backend/src/main/java/com/backend/backend/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://proyectoar-35592.web.app")
 public class CEducacion {
 
     @Autowired
